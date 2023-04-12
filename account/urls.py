e8667d166d6344346414a659151ef188a222d4ed
dkdnl <dkdnl369@gmail.com>
2023-04-12 11:07:41 +0900

    profiles

---
+++ account/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+# app_name = 'profiles'
+
+urlpatterns = [
+    path('', views.profile_list, name='profile_list'),
+    path('new/', views.profile_new, name='profile_new'),
+    # path('<int:pk>/', views.profile_detail, name='profile_detail'),
+    # path('<int:pk>/edit/', views.profile_edit, name='profile_edit'),
+    path('profile/', views.profile_detail, name='profile_detail'),
+    path('profile/edit/', views.profile_edit, name='profile_edit'),
+]
+
