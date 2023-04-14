51737d4c139effa56e38f1853ed07cdfc2d85101
geunhye-kim <mykgh6514@naver.com>
2023-04-14 11:26:14 +0900

    my post on mypage

--- account/views.py
+++ account/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 from .models import Profile
 from .forms import ProfileForm
 
 
 def profile(request):
-    user = request.user
-    count = user.mypost.filter(user_id=user.id).count()
-    print(count)
-    return render(request, 'account/profile.html', {'user': user, 'mypost_count': count})
+    if request.method == 'GET':
+        user = request.user
+        count = user.mypost.filter(user_id=user.id).count()
+        post = user.mypost.filter(user_id=user.id)
+        paginator = Paginator(post, per_page=8)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'account/profile.html', {'user': user, 'mypost_count': count, 'page_obj': page_obj})
 
 
 def profile_edit(request):
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,21 +3,20 @@ function notuser() {
     alert('권한이 없습니다');
 }
 
-function edit(id){
-    let comment_data = document.querySelector('#comment'+id)
-    document.querySelector('.comment_control'+id).remove()
+function edit(id) {
+    let comment_data = document.querySelector('#comment' + id)
+    document.querySelector('.comment_control' + id).remove()
     content = comment_data.textContent
     comment_data.remove()
-    let edit_comment = document.querySelector('#comment_edit'+id)
-    
-    edit_comment.innerHTML=
-    "<p id='comment{{comment.id}}' class='card-text'>"+
-    "<input class='test'type='text' name='comment' value='"+content+"'>"+
-    "</input>"+
-    "</p>"+
-    "<div style='float: right'>"+
-    "<input type='hidden' name='csrfmiddlewaretoken' value='"+document.getElementsByName('csrfmiddlewaretoken')[0].value+"'>" + 
-    "<input type='submit' class='btn btn-outline-dar' value='수정'></input>"+
-    "</div>"
-    
+    let edit_comment = document.querySelector('#comment_edit' + id)
+
+    edit_comment.innerHTML =
+        "<p id='comment{{comment.id}}' class='card-text'>" +
+        "<input class='comm' type='text' name='comment' value='" + content + "'>" +
+        "</input>" +
+        "</p>" +
+        "<div style='float: right'>" +
+        "<input type='hidden' name='csrfmiddlewaretoken' value='" + document.getElementsByName('csrfmiddlewaretoken')[0].value + "'>" +
+        "<input type='submit' class='btn btn-outline-dar' value='수정'></input>" +
+        "</div>"
 }
